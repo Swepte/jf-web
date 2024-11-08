09b1380f9fb316271b3b73166dd464dfbb811e13
Jamille <jamille.legaspi@cryptex.ph>
2024-11-08 13:33:22 +0800

    utils http

---
+++ src/utils/http.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+export const i = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL });
+i.interceptors.request.use(async (request) => {
+  return request;
+});
+
+export async function get(url: string) {
+  const res = await i.get(url);
+
+  return res;
+}
+
+export async function patch(url: string, data?: unknown) {
+  const res = await i.patch(url, data);
+
+  return res;
+}
+
+export async function put(url: string, data?: unknown) {
+  const res = await i.put(url, data);
+
+  return res;
+}
+
+export async function del(url: string, data?: unknown) {
+  const res = await i.delete(url, { data });
+  return res;
+}
+
+export async function post<T>(url: string, data: unknown) {
+  const res = await i.post<T>(url, data);
+  return res;
+}
+
+// export async function postForm<T>(url: string, data?: any) {
+//   const res = await i.post<T>(url, data, {
+//     headers: {
+//       "Content-Type": "multipart/form-data",
+//     },
+//     method: "POST",
+//   });
+//   return res;
+// }
